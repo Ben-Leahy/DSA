bda6fb84e470fc5747a3fb90e4c177e4f4f09309
Ben Leahy <benleahy41@gmail.com>
2026-09-02 13:53:01 +1000

    Draft 1 of Huffmans
    -- We have created functions to calculate frequencies from a string
    -- Change this to a list of nodes we can use for Huffmans
    -- Huffmans algorithm
    
    We still need to:
    -- chnage self.edges to self.left and self.right
    -- create decoding table

--- 3155/compression/huffman.py
+++ 3155/compression/huffman.py
@@ -1,25 +1,78 @@
-"""Reflectoin from class. Just follow the class and if there are other things ii want to investigate note them down and do them later."""
-class Node:
-    def __init__():
-        #is leaf
-        # freq?
-        #edges []
-        pass
+from __future__ import annotations
 
-class edge: 
-    def __init__():
-        pass
+"""Reflectoin from class. Just follow the class and if there are other things ii want to investigate note them down and do them later."""
+
+# How to encode the tree vs the sorted array that we need to refer to?
+# How to deal with binary numbers?
+class Edge: 
+    def __init__(self, val:int, to_node:Node):
+        self.val = val
+        self.to_node = to_node
     # 1 or 0
 
-def huffman(sorted_freqs: list[int]) -> list[int]: # -> Node
+# Change edges to self.left, self.right
+class Node:
+    def __init__(self, title:str, freq:int,  is_leaf:bool = False, edges:list[Edge] = []) -> None:
+        self.title = title
+        self.freq = freq
+        self.is_leaf = is_leaf
+        self.edges = edges
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+    def __le__(self, other):
+        return self.freq <= other.freq
+
+
+def frequencies(txt: str) -> list[int]:
+    freqs = [0]*128
+    for char in txt:
+        freqs[ord(char)] += 1
+    return freqs
+
+def unique_frequencies(freqs: list[int]) -> list[int]:
+    unique_freqs = []
+    for freq in freqs:
+        if freq != 0:
+            unique_freqs.append(freq)
+    return unique_freqs
+
+def create_sorted_nodes(unique_freqs: list[int]) -> list[Node]:
+    nodes:list[Node] = []
+    for i, freq in enumerate(unique_freqs):
+        if freq != 0:
+            nodes.append(Node(chr(i), freq))
+    nodes.sort()
+    return nodes
+
+
+def huffman(nodes: list[Node]) -> Node | None:
     """
     Construct a deterministic variable length encoding for characters. 
     This is necessary to compress - we can store more common examples with shorter characters. 
     This needs to be prefix free to go in both directions
     The basic idea is to store each encoding as a path to a leaf in a strict binary tree. This way no leaf is a prefix of another leaf.
     This generates the decoding graph.
+    :input: a list of tuples: [frequency, char/string, ]
     :output: root node of the graph"""
-    pass
+    """Do we assume that our alphabet is om"""
+    # TODO this doesn't work for nodes of length less than 2
+    root = None
+    while len(nodes) >= 2:
+        # Select top 2 
+        title = nodes[0].title + nodes[1].title
+        freq = nodes[0].freq + nodes[1].freq
+        is_leaf = False
+        edges = [Edge(0, nodes[0]), Edge(1, nodes[1])]
+        root = Node(title, freq, is_leaf, edges)
+
+        nodes = nodes[2:]
+        # Change this to a bubble sort so that it had better time complexity. 
+        nodes.append(root)
+        nodes.sort()
+
+    return root
     # While sorted_freqs length >= 2:
         # Select top 2, sorted_freqs[0], sorted_freqs[1]
         # Create a new node that points to them: 0 to the one with the lower freq, with freq= sorted_freqs[0] + sorted_freqs[1]
@@ -46,9 +99,9 @@ def encoding_table(root: Node) -> list[int]:
     # Base case:
         # if we are at a leaf, then return the string passed in. 
 
-def frequencies(ascii_start: int, ascii_end: int, txt: str) -> list[int]:
-    """Generate an array of size alphabet, that stores the frequency of each character"""
-    pass
+# def frequencies(ascii_start: int, ascii_end: int, txt: str) -> list[int]:
+#     """Generate an array of size alphabet, that stores the frequency of each character"""
+#     pass
 
 def sorted_frequencies(freqs: list[int]) -> list[int]:
     pass
---
+++ 3155/compression/test.py
@@ -0,0 +1,5 @@
+my_array = ["hello", "ben"]
+def foo(array):
+    array.sort()
+    return array
+print(foo(my_array))
